ecd0ac1b8c6914e4661db4c3ad86d94adbb721c5
shubh <shubhspj@gmail.com>
2020-11-14 14:24:17 +0530

    Spring_DI_constructortype

--- src/com/shubham/springdemo/TrackCoach.java
+++ src/com/shubham/springdemo/TrackCoach.java
@@ -2,6 +2,15 @@ package com.shubham.springdemo;
 
 public class TrackCoach implements Coach {
 
+	
+	
+private FortuneService fortuneService;
+	
+public TrackCoach() {
+	}
+	public TrackCoach(FortuneService thefortuneservice) {
+		fortuneService=thefortuneservice;
+	}
 	@Override
 	public String getDailyWorkout() {
 		// TODO Auto-generated method stub
@@ -11,7 +20,7 @@ public class TrackCoach implements Coach {
 	@Override
 	public String getDailyFortune() {
 		// TODO Auto-generated method stub
-		return null;
+		return "That is insane!!You are champion!"+fortuneService.getFortune();
 	}
 
 }
